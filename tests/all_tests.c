fd7efab3c8159a0dab598bd58daef56391f66134
ldalzotto <loic.dalzotto@hotmail.fr>
2025-09-13 10:37:56 +0200

    Remove headless window management module and related tests
    This commit deletes the `win_headless` implementation and its associated header files, as well as the corresponding test files. The Makefile has been updated to remove references to these files, streamlining the project by eliminating unused components.

--- tests/all_tests.c
+++ tests/all_tests.c
@@ -2,7 +2,6 @@
 #include "test_mem.h"
 #include "test_stack_alloc.h"
 #include "test_win_x11.h"
-#include "test_win_headless.h"
 #include <stdio.h>
 
 int main() {
@@ -17,7 +16,6 @@ int main() {
     test_mem_module(&ctx);
     test_sa_module(&ctx);
     test_win_x11_module(&ctx);
-    test_win_headless_module(&ctx);
 
     // Report results
     test_report_context(&ctx);
--- tests/test_win_headless.h
+++
@@ -1,9 +0,0 @@
-#ifndef TEST_WIN_HEADLESS_H
-#define TEST_WIN_HEADLESS_H
-
-#include "test_framework.h"
-
-// Test function declarations for win_headless module
-void test_win_headless_module(test_context* t);
-
-#endif /* TEST_WIN_HEADLESS_H */
