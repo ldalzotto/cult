ed18ebb2075eb7fe8831e4e126e242eef51cf5a6
ldalzotto <loic.dalzotto@hotmail.fr>
2025-09-14 13:56:28 +0200

    feat(test): add test for nested struct printing to file
    - Added complex_t struct with nested test_point_t and id fields
    - Defined field descriptors and print_meta for complex_t
    - Implemented test_print_nested_to_file to verify printing nested structs to a file
    - Updated test_print_module to include the new test
    
    This ensures the generic printing functionality correctly handles nested structures when outputting to files.

--- tests/test_print.c
+++ tests/test_print.c
@@ -12,11 +12,16 @@
 // Path definitions using STATIC_STRING
 static const string_span path_test_output = STATIC_STRING("test_temp/print_test_output.txt");
 
-// Define a test struct
+// Define test structs
 typedef struct {
     i32 x;
     i32 y;
 } test_point_t;
+
+typedef struct {
+    test_point_t pos;
+    i32 id;
+} complex_t;
 
 // Fields for test_point_t
 field_descriptor test_point_fields[] = {
@@ -32,6 +37,22 @@ print_meta test_point_meta = {
     .pt = PT_NONE,
     .fields_begin = test_point_fields,
     .fields_end = test_point_fields + (sizeof(test_point_fields) / sizeof(field_descriptor))
+};
+
+// Fields for complex_t
+field_descriptor complex_fields[] = {
+    {"pos", offsetof(complex_t, pos), &test_point_meta},
+    {"id", offsetof(complex_t, id), &i32_meta}
+};
+
+// Meta for complex_t
+print_meta complex_meta = {
+    .type_name = "complex_t",
+    .is_array = 0,
+    .type_size = sizeof(complex_t),
+    .pt = PT_NONE,
+    .fields_begin = complex_fields,
+    .fields_end = complex_fields + (sizeof(complex_fields) / sizeof(field_descriptor))
 };
 
 void test_print_primitives(test_context* t) {
@@ -104,11 +125,52 @@ void test_print_to_file(test_context* t) {
     TEST_ASSERT_TRUE(t, 1);
 }
 
+void test_print_nested_to_file(test_context* t) {
+    // Allocate memory using mem_map
+    const uptr stack_size = 1024;
+    void* memory = mem_map(stack_size);
+    stack_alloc alloc;
+    sa_init(&alloc, memory, byteoffset(memory, stack_size));
+
+    // Define test data
+    complex_t obj = {{10, 20}, 42};
+    const char* expected = "complex_t {\n  pos: test_point_t {\n    x: 10\n    y: 20\n  }\n  id: 42\n}";
+
+    // Open file for writing
+    file_t file = file_open(&alloc, path_test_output.begin, path_test_output.end, FILE_MODE_WRITE);
+    TEST_ASSERT_NOT_NULL(t, file);
+
+    // Print to file using file_t version
+    print_generic(&complex_meta, &obj, file, 0);
+    file_close(file);
+
+    // Open file for reading
+    file_t read_file = file_open(&alloc, path_test_output.begin, path_test_output.end, FILE_MODE_READ);
+    TEST_ASSERT_NOT_NULL(t, read_file);
+
+    // Read content
+    void* buffer;
+    uptr size = file_read_all(read_file, &buffer, &alloc);
+    TEST_ASSERT_TRUE(t, size > 0);
+
+    // Check content
+    TEST_ASSERT_TRUE(t, strncmp((char*)buffer, expected, strlen(expected)) == 0);
+
+    sa_free(&alloc, buffer);
+    file_close(read_file);
+
+    sa_deinit(&alloc);
+    mem_unmap(memory, stack_size);
+
+    TEST_ASSERT_TRUE(t, 1);
+}
+
 void test_print_module(test_context* t) {
     setup_test_temp_dir();
     test_print_primitives(t);
     test_print_struct(t);
     test_print_array(t);
     test_print_to_file(t);
+    test_print_nested_to_file(t);
     cleanup_test_temp_dir();
 }
