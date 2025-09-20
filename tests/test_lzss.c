004b3eac7ee4e86317e9134e4eb553ee83fe1159
ldalzotto <loic.dalzotto@hotmail.fr>
2025-09-20 12:40:53 +0200

    refactor: remove redundant test_lzss function from LZSS tests
    The test_lzss function, which performed basic LZSS compression and decompression on a repetitive string, has been removed as it was redundant with other more comprehensive tests in the file. This change streamlines the test suite without losing coverage.

--- tests/test_lzss.c
+++ tests/test_lzss.c
@@ -4,39 +4,6 @@
 #include "coding/lzss.h"
 #include <string.h>
 
-static void test_lzss(test_context* t) {
-    uptr size = 1024 * 1024;
-    void* mem = mem_map(size);
-    TEST_ASSERT_NOT_NULL(t, mem);
-
-    stack_alloc alloc;
-    sa_init(&alloc, mem, byteoffset(mem, size));
-
-    string input = STR("hello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_worldhello_world");
-    uptr input_size = bytesize(input.begin, input.end);
-    TEST_ASSERT(t, input_size > 0, "Input size should be positive");
-
-    lzss_config config;
-    config.match_size_max = 255;
-    config.match_size_min = 3;
-    config.window_size_max = 1024;
-    void* out = lzss_compress((u8*)input.begin, (u8*)input.end, config, &alloc, 0);
-    TEST_ASSERT_NOT_NULL(t, out);
-
-    uptr compressed_size = bytesize(out, alloc.cursor);
-    TEST_ASSERT(t, compressed_size > 0, "Compressed size should be positive");
-    TEST_ASSERT(t, compressed_size < input_size, "Compressed size should be smaller than input for repetitive data");
-
-    void* decompressed = lzss_decompress(out, alloc.cursor, &alloc, 0);
-    uptr decompressed_size = bytesize(decompressed, alloc.cursor);
-    TEST_ASSERT(t, decompressed_size == input_size, "");
-
-    sa_free(&alloc, out);
-
-    sa_deinit(&alloc);
-    mem_unmap(mem, size);
-}
-
 static void test_lzss_window_size_boundary(test_context* t) {
     uptr size = 64 * 1024;
     void* mem = mem_map(size);
@@ -295,7 +262,6 @@ static void test_lzss_config_variations(test_context* t) {
 
 void test_lzss_module(test_context* t) {
     print_string(file_stdout(), STRING("Registering LZSS Module Tests...\n"));
-    REGISTER_TEST(t, "lzss", test_lzss);
     REGISTER_TEST(t, "lzss_window_size_boundary", test_lzss_window_size_boundary);
     REGISTER_TEST(t, "lzss_large_input", test_lzss_large_input);
     REGISTER_TEST(t, "lzss_all_identical", test_lzss_all_identical);
