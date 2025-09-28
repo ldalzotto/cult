eb3e5a6e0f49b0591344542a344285b3d759363e
ldalzotto <loic.dalzotto@hotmail.fr>
2025-09-28 17:48:26 +0200

    test(snake): add boundary-left-top rendering test and register
    - Introduces test_snake_boundary_left_top to verify snake rendering clamps at the left/top boundaries
    - Simulates left/up movement to ensure head is clamped at (0,0) and reward is rendered at expected pixel coordinates (30,30)
    - Registers the new test in test_snake_module to include it in the test suite

--- tests/test_snake.c
+++ tests/test_snake.c
@@ -38,6 +38,7 @@ void test_render(test_context* t) {
     mem_unmap(pointer, size);
 }
 
+
 // New test: move the snake towards the reward and verify the reward is rendered at an expected position.
 // Initial conditions based on snake_init: head starts at (10,10) in grid, reward at (6,6) in grid.
 // We perform 2 left moves and 2 up moves to end at (8,8). With screen 100x100 and grid 20x20,
@@ -93,7 +94,59 @@ void test_snake_move_towards_reward(test_context* t) {
     mem_unmap(pointer, size);
 }
 
+// New test: boundary left/top
+// Move the snake left and then up to reach the (0,0) boundary and verify rendering coordinates are clamped.
+void test_snake_boundary_left_top(test_context* t) {
+    uptr size = 1024 * 1024;
+    void* pointer = mem_map(size);
+    stack_alloc _alloc;
+    stack_alloc* alloc = &_alloc;
+    sa_init(alloc, pointer, byteoffset(pointer, size));
+    snake* s = snake_init(alloc);
+
+    // Move left towards the left boundary (0,0)
+    for (int i = 0; i < 20; ++i) {
+        snake_input input = {0};
+        input.left = 1;
+        snake_update(s, input, 16, alloc);
+    }
+    // Move up towards the top boundary
+    for (int i = 0; i < 20; ++i) {
+        snake_input input = {0};
+        input.up = 1;
+        snake_update(s, input, 16, alloc);
+    }
+
+    u32 command_count;
+    draw_command* cmds = snake_render(s, 100, 100, &command_count, alloc);
+
+    // We expect head clamped at (0,0) and reward at initial (6,6) -> (30,30) in pixels
+    TEST_ASSERT_EQUAL(t, command_count, 3);
+
+    TEST_ASSERT_EQUAL(t, cmds[0].type, DRAW_COMMAND_CLEAR_BACKGROUND);
+
+    TEST_ASSERT_EQUAL(t, cmds[1].type, DRAW_COMMAND_DRAW_RECTANGLE);
+    TEST_ASSERT_EQUAL(t, cmds[1].data.rect.x, 0);
+    TEST_ASSERT_EQUAL(t, cmds[1].data.rect.y, 0);
+    TEST_ASSERT_EQUAL(t, cmds[1].data.rect.w, 5);
+    TEST_ASSERT_EQUAL(t, cmds[1].data.rect.h, 5);
+    TEST_ASSERT_EQUAL(t, cmds[1].data.rect.color, 0x00FFFFFF);
+
+    TEST_ASSERT_EQUAL(t, cmds[2].type, DRAW_COMMAND_DRAW_RECTANGLE);
+    TEST_ASSERT_EQUAL(t, cmds[2].data.rect.x, 30);
+    TEST_ASSERT_EQUAL(t, cmds[2].data.rect.y, 30);
+    TEST_ASSERT_EQUAL(t, cmds[2].data.rect.w, 5);
+    TEST_ASSERT_EQUAL(t, cmds[2].data.rect.h, 5);
+    TEST_ASSERT_EQUAL(t, cmds[2].data.rect.color, 0x0000FF00);
+
+    snake_deinit(s, alloc);
+
+    sa_deinit(alloc);
+    mem_unmap(pointer, size);
+}
+
 void test_snake_module(test_context* t) {
     REGISTER_TEST(t, "snake_render", test_render);
     REGISTER_TEST(t, "snake_move_towards_reward", test_snake_move_towards_reward);
+    REGISTER_TEST(t, "snake_boundary_left_top", test_snake_boundary_left_top);
 }
