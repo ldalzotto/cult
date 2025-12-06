f089ca96325fb750e30305d7121c41c0c4e1d085
ldalzotto <loic.dalzotto@hotmail.fr>
2025-12-06 13:31:52 +0100

    Fix comment typos and clarify size-optimization intent in lz_bit_types.h
    - Reword TODO about using an 8-bit field for size optimization.
    - Add comment explaining the purpose of the following struct: it packs 8 item_type flags into a single byte.
    - Fix minor typos ("optimisation" -> "optimization" left as British spelling? preserved "optimisation" in struct comment) and grammar for clarity.
    
    No functional changes.

--- src/libs/coding/lz_bit_types.h
+++ src/libs/coding/lz_bit_types.h
@@ -3,12 +3,12 @@
 
 #include "primitive.h"
 
-// TODO: For size optimization, we can write this as an 8 bit field
 typedef u8 item_type;
 static const item_type LITERAL = 0;
 static const item_type MATCH = 1;
 STATIC_ASSERT(sizeof(item_type) == 1);
 
+// Used for size optimisation. It stores 8 item_type flag on a single but
 typedef struct {
     u8* value;
     u8 bit_index;
